c96e9d3f6a341a47a38f03f8e0b5efb2c2ae7bbc
RobertJulio <cholijulio46@gmail.com>
2026-08-27 22:01:54 -0500

    Uso de hooks y navegacion

--- src/screens/ClasesScreen.js
+++ src/screens/ClasesScreen.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, Text, textInput, FlatList, ScrollView, StyleSheet, TextInput } from 'react-native';
+import NivelFiltro from '../components/NivelFiltro';
 import Card from 'control-gastos/src/components/Card.js';
 import {spacing, colors, typography} from '../theme';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
@@ -9,6 +10,7 @@ import {CLASES, NIVELES} from '../data/clases'
 const ClasesScreen = ({navigation}) => {
     //const { columnas, paddingHorizontal } = useResposive;
     const [nivel, setNivel] = useState('Todos');
+    const [busqueda, setBusqueda] = useState('');
 
   return (
     <View>
@@ -18,11 +20,33 @@ const ClasesScreen = ({navigation}) => {
           <Ionicons name="search" size={18} />
           <TextInput 
             placeholder="Buscar por nivel o profesor"
-            value={nivel}
-            onChangeText={setNivel}
+            value={busqueda}
+            onChangeText={setBusqueda}
             autoCorrect = {false}
           />
+
+          {busqueda.length > 0 && (
+            <Ionicons name="close-circle" 
+            size={18} 
+            onPress={() => setBusqueda('')} />
+          )}
         </View>
+        <ScrollView>
+          style={{flexgrow: 0}}
+          
+          //repasar el metodo .map de js
+          {
+            NIVELES.map((item)=>(
+              <NivelFiltro 
+                eiqueta={item}
+                activo={nivel === item}
+                onPress={ () => setNivel(item)}
+              />
+            ))
+          }
+    
+        </ScrollView>
+
       </View>
     </View>
 
